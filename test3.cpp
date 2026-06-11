da0b975e756a8cd3da9f77dd5f44f825740fdc5a
hgc-qwe <1410919772@qq.com>
2026-06-11 21:48:51 +0800

    set name

---
+++ test3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <hiredis/hiredis.h>
+using namespace std;
+
+int main() {
+    redisContext* ctx = redisConnect("127.0.0.1", 6379);
+    if (ctx == nullptr) {
+        cout << "connect failed" << endl;
+        return -1;
+    }
+
+    if (ctx->err) {
+        cout << ctx->errstr << endl;
+        redisFree(ctx);
+        return -1;
+    }
+
+    cout << "connect success" << endl;
+
+    redisReply* reply = (redisReply*)redisCommand(ctx, "SET name George");
+    cout << reply->str << endl;
+
+    freeReplyObject(reply);
+    redisFree(ctx);
+
+    return 0;
+}
